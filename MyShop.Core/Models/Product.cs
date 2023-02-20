1cd1b0e53966a8629a5dc67a17f46fd6f94e6afe
TechJerryyy <124981826+TechJerryyy@users.noreply.github.com>
2023-02-20 15:52:40 +0530

    Project Manager Pages Built and test

--- MyShop.Core/Models/Product.cs
+++ MyShop.Core/Models/Product.cs
@@ -15,13 +15,21 @@ namespace MyShop.Core.Models
         [DisplayName("Product Name")]
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Price { get; set; }
+        [Range(0, 1000)]
+        public Decimal Price { get; set; }
+
+        [DefaultValue(0.00)]
         public string Category { get; set; }
         public string Image { get; set; }
         
         public Product()
         {
             this.Id = Guid.NewGuid().ToString();
+        }
+
+        public Product Find(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ MyShop.Core/Models/RangeAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyShop.Core.Models
+{
+    internal class RangeAttribute : Attribute
+    {
+        public RangeAttribute(int v1, int v2)
+        {
+            V1 = v1;
+            V2 = v2;
+        }
+
+        public int V1 { get; }
+        public int V2 { get; }
+    }
+}
