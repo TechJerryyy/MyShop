09b26b5a1d089e8bff0d60a9594e9197de70fbba
TechJerryyy <124981826+TechJerryyy@users.noreply.github.com>
2023-02-22 11:05:54 +0530

    Added Entity Framework

--- MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T : BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items;
@@ -18,7 +19,7 @@ namespace MyShop.DataAccess.InMemory
         {
             className = typeof(T).Name;
             items = cache[className] as List<T>;
-            if(items==null)
+            if (items == null)
             {
                 items = new List<T>();
             }
@@ -27,6 +28,7 @@ namespace MyShop.DataAccess.InMemory
         {
             cache[className] = items;
         }
+        
         public void Insert(T t)
         {
             items.Add(t);
@@ -35,7 +37,7 @@ namespace MyShop.DataAccess.InMemory
         {
             T tToUpdate = items.Find(i => i.Id == t.Id);
 
-            if(tToUpdate!=null)
+            if (tToUpdate != null)
             {
                 tToUpdate = t;
             }
@@ -44,7 +46,7 @@ namespace MyShop.DataAccess.InMemory
                 throw new Exception(className + "Not Found");
             }
         }
-        public T Find (string Id)
+        public T Find(string Id)
         {
             T t = items.Find(i => i.Id == Id);
             if (t != null)
